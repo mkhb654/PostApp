82b395e37acf48cec2cd41b8ca72857f9175dfdf
Muntaz <mkhb654@gmail.com>
2014-12-16 14:34:51 -0500

    Updated the post item

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -2,6 +2,9 @@
  * Created by mkaleem on 12/15/14.
  */
 Template.postItem.helpers({
+    ownPost: function () {
+        return this.userId === Meteor.userId();
+    },
     domain: function() {
         var a = document.createElement('a');
         a.href = this.url;
